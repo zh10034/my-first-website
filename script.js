e94a1e6c8ba1376367fe6ab187a1388b20c14260
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-11-17 18:33:09 -0500

    onclick toolbar

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JS";
 document.querySelector('body').appendChild(h2);
+
+function scrollToSection(Home) {
+    const section = document.getElementById(Home);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  function scrollToSection(About) {
+    const section = document.getElementById(About);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  function scrollToSection(Contact) {
+    const section = document.getElementById(Contact);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
